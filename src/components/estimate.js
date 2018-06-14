80965e4d6de2f4f7be47d483713ad1cc184c47c9
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-14 21:11:49 +0530

    Red button estimate added

--- src/components/estimate.js
+++ src/components/estimate.js
@@ -12,6 +12,37 @@ const Estimate = (props) => {
                 <figure className="bikeImg">
                     <img src="../../images/Bike.png" alt="The Pulpit Rock" width="304" height="228"/>
                 </figure>
+                <div className="estimate-form">
+                    <form className="form-inline" action="#">
+                        <div className="form-group">
+                            SELECT
+                            {/*<select className="custom-select">*/}
+                                {/*<option selected>Open this select menu</option>*/}
+                                {/*<option value="1">One</option>*/}
+                                {/*<option value="2">Two</option>*/}
+                                {/*<option value="3">Three</option>*/}
+                            {/*</select>*/}
+
+                        </div>
+                        <div className="form-group">
+                            SELECT
+                            {/*<select className="custom-select" value="Your Parcel Details">*/}
+                                {/*<option selected>Open this select menu</option>*/}
+                                {/*<option value="1">One</option>*/}
+                                {/*<option value="2">Two</option>*/}
+                                {/*<option value="3">Three</option>*/}
+                            {/*</select>*/}
+                        </div>
+                        <span className="Button">
+                            <a href="#">
+                                <button className="RedButton">
+                                    PRICE ESTIMATE
+                                </button>
+                            </a>
+                        </span>
+                    </form>
+
+                </div>
             </div>
 
         </section>
