7432c1cf4242b6ebe9267812f59bb165439cb7c3
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-14 19:36:34 +0530

    image added

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from './header';
+import Estimate from './estimate';
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <Estimate />
       </div>
     );
   }
---
+++ src/components/estimate.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Auth from '../containers/Auth'
+
+const Estimate = (props) => {
+
+    return (
+        <section className="Estimate">
+            <div className="container">
+                <div className="estimate-label">
+                    Bike Shipping Through Railways Got Convenient and Easy
+                </div>
+                <figure className="bikeImg">
+                    <img src="../../images/Bike.png" alt="The Pulpit Rock" width="304" height="228"/>
+                </figure>
+            </div>
+
+        </section>
+    )
+};
+
+export default Estimate;
